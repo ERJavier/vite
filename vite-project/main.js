61058d064b77402b54f79f4c58912d053d78d6c7
Javier Cepeda <erjavierc@gmail.com>
2022-08-13 11:29:10 -0400

    vite

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,8 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import { setupCounter } from './counter.js';
+import buttonSyles from './button.module.css';
+import imagen1 from './vite2.svg';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -10,6 +12,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
+    <button id=btn>Click</button>
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
@@ -18,6 +21,9 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
+  <img id="img" />
 `
-
-setupCounter(document.querySelector('#counter'))
+document.getElementById('btn').className = buttonSyles.btn;
+setupCounter(document.querySelector('#counter'));
+const imagen = document.getElementById('img');
+imagen.src = `${imagen1}`;

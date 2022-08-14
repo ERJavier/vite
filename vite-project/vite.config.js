d50316c6b01c4cdc5cafc573dc99049720103ff6
Javier Cepeda <erjavierc@gmail.com>
2022-08-14 08:04:29 -0400

    vite

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 export default defineConfig(({command, mode}) => {
     const port = 2020
 
     console.log(command, mode)
+    const env = loadEnv(mode, process.cwd())
     
+    if (mode === "development") {
+        console.log("modo desarrollo")
+    }else{
+        console.log("modo produccion")
+    }
+
     return {
         server: {
             port
